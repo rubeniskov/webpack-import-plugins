1f6e608db8e038bfe2753736389c84e145ef6cad
rubeniskov <me@rubeniskov.com>
2020-11-04 18:41:34 +0100

    fix: missing node.NodeTargetPlugin plugin in wepack@4 plugin list

--- scripts/gen-plugin-list.sh
+++ scripts/gen-plugin-list.sh
@@ -19,17 +19,19 @@ collect_webpack_plugins() {
         [...p,...(/Plugin$/.test(c)?[x?x+'.'+c:c]:(typeof o[c]==='object'?u(o[c],c):[]))],[]);\
         u(require(\"./tmp/webpack@$1/\")).map((v)=>console.log(v))" 2>/dev/null \
     | xargs -L 1 -I {} \
-        bash -c 'echo $1 $(find "tmp/webpack@$2" -type f -name "${1##*.}.js"|sed -e "s/^tmp\/webpack@$2\///")' -a {} $1 \
-    | sort -k1
+        bash -c 'echo $1 $(find "tmp/webpack@$2" -type f -name "${1##*.}.js"|sed -e "s/^tmp\/webpack@$2\///")' -a {} $1
     rm -rf tmp
 }
 
 format_plugin_json() { 
-    jq --slurp --raw-input 'split("\n")[:-1]|map({name: split(" ")[0], filename: split(" ")[1]})'
+    sort -k1 | jq --slurp --raw-input 'split("\n")[:-1]|map({name: split(" ")[0], filename: split(" ")[1]})'
 }
 
-
 echo >&2 "Generating webpack@4 plugins list"
-collect_webpack_plugins 4 | format_plugin_json > wp-plugins-v4.json
+# Manual insertion for unreferenced plugins in webpack 4
+cat <(collect_webpack_plugins 4) <(cat <<EOF
+node.NodeTargetPlugin lib/node/NodeTargetPlugin.js
+EOF
+) | format_plugin_json > wp-plugins-v4.json
 echo >&2 "Generating webpack@5 plugins list"
 collect_webpack_plugins 5 | format_plugin_json > wp-plugins-v5.json
